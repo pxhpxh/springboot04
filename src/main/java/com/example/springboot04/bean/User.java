c73a92830382afd106bb6f291282796be4d64b01
32947 <329478916@qq.com>
2024-01-23 16:53:12 +0800

    初始化

--- src/main/java/com/example/springboot04/bean/User.java
+++ src/main/java/com/example/springboot04/bean/User.java
@@ -26,7 +26,6 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
 
-
     @TableId(value = "id", type = IdType.AUTO)
     @ApiModelProperty("id")
     private Integer id;
